cf9ac82c9db90b1e21637274161b39d055988164
Peter Kovacik <pkovacik2@my.wctc.edu>
2020-10-03 20:58:55 -0500

    changes to MovieFile

--- MovieFile.cs
+++ MovieFile.cs
@@ -13,7 +13,30 @@ namespace MediaLibrary
         private static NLog.Logger logger = NLogBuilder.ConfigureNLog(Directory.GetCurrentDirectory() + "\\nlog.config").GetCurrentClassLogger();
 
         public MovieFile(string movieFilePath) {
+            filePath = movieFilePath;
+            Movies = new List<Movie>();
 
+            try {
+                StreamReader sr = new StreamReader(filePath);
+                while (!sr.EndOfStream)
+                {
+                    Movie movie = new Movie();
+                    string line = sr.ReadLine();
+
+                    // string[] movieDetails = line.Split(',');
+                    // movie.movieId = UInt64.Parse(movieDetails[0]);
+                    // movie.title = movieDetails[1];
+                    // movie.genres = movieDetails[2].Split('|').ToList();
+
+                    Movies.Add(movie);
+                }
+                sr.Close();
+                logger.Info("Movies in file {Count}", Movies.Count);
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+            }
         }
 
         public bool isUniqueTitle(string title)
--- Program.cs
+++ Program.cs
@@ -15,7 +15,37 @@ namespace MediaLibrary
             string movieFilePath = Directory.GetCurrentDirectory() + "\\movies.scrubbed.csv";
             MovieFile movieFile = new MovieFile(movieFilePath);
 
-            
+            string choice;
+            do {
+                Console.WriteLine("Enter 1 to add movie.");
+                Console.WriteLine("Enter 2 to display all movies.");
+                Console.WriteLine("Enter anything else to quit.");
+                choice = Console.ReadLine();
+
+                if (choice == "1") {
+                    // add movies
+
+                    /*
+                        1.) add title
+                        2.) check to see if title is unique - if (movieFile.isUniqueTitle(movie.title))
+                        3.) enter genre(s) (add no genres listed if none)
+                        4.) enter director
+                        5.) enter running time - public TimeSpan runningTime
+                        -------------------
+                        Add movie to list - movieFile.AddMoive(movie);
+                    */
+
+                }
+                else if (choice == "2") {
+                    // display all movies
+                    // ----------------------
+                    // foreach(Movie m in movieFile.Movies)
+                    // {
+                    //     Console.WriteLine(m.Display());
+                    // }
+                }
+            } while (choice == "1" || choice == "2");
+
 
             logger.Info("Program ended");
         }
